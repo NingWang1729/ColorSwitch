3ada8240d7f45b9f51d801a71a7339393f29c17c
Ning Wang <ningwang1729@gmail.com>
2021-08-12 21:37:15 -0700

    Cleared up original template

---
+++ ColorSwitch/Scenes/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  ColorSwitch
+//
+//  Created by Ning Wang on 8/12/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        
+    }
+    
+}
